ee2c8f8d1af29a4fdc35ead5285ed4a4c5e9c5e4
Maxi Rios <maxirios@protonmail.com>
2023-06-22 04:27:34 -0300

    Setup for Webpack, Babel and React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+    console.log('App');
+    return (
+        <>
+            <div>
+                <h1>Cotización del dólar</h1>
+            </div>
+        </>
+    )
+}
+
+export default App
